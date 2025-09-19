75232dd45af9822f2617350f0f501e1c8323e762
I-am-Jellycaramel <northwind3552@gmail.com>
2025-09-19 14:48:09 +0900

    feat: let, const 구분

---
+++ week3/test.js
@@ -0,0 +1,11 @@
+const startingPokerChips = 100;
+const players = 3;
+const noOfStarterCards = 2;
+
+let player1Point = startingPokerChips;
+let player2Point = startingPokerChips;
+let player3Point = startingPokerChips;
+
+player1Point -= 50;
+player2Point -= 25;
+player3Point += 75;
